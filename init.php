9f1185d7da559a8b8eec0a3922438568f141cc1a
Daniel Gryniewicz <dang@fprintf.net>
2013-03-21 09:11:48 -0400

    Remove trailing whitespace

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 // Plugin: dangkeys
 //    provides a hotkey plugin that can be customized to your site
 //
-// By default, this plugin does nothing. 
+// By default, this plugin does nothing.
 // You must enable it, and add your own hotkey maps.
 // (Why? So future updates will not overwrite your customized init.php.
 //  They will only update the init.php-dist file.)
@@ -15,11 +15,11 @@
 //
 // Finally, reload your tt-rss web session. Enable the plugin in
 //   Actions -> Preferences -> Plugins -> User Plugins.
-// 
+//
 
 // What if you want per-user hotkeys?
 //     There's currently no way for a user to customize their own keys.
-//     You can provide multiple hotkey plugins, if you like. 
+//     You can provide multiple hotkey plugins, if you like.
 //     To do so:
 //         make a new plugins/user1_keys directory
 //         copy this init.php file to that directory
@@ -43,7 +43,7 @@ class DangKeys extends Plugin {
 		$host->add_hook($host::HOOK_HOTKEY_MAP, $this);
 	}
 
-	// This is where the hotkey maps are defined. You can uncomment 
+	// This is where the hotkey maps are defined. You can uncomment
 	// the example codes below, or add your own.
 	//
 	// Each map looks like this:
@@ -57,18 +57,18 @@ class DangKeys extends Plugin {
 	//      "(37)|left"     = a javascript key code and label
 	//      "^(38)|Ctrl-Up" = can use * Shift or ^ Ctrl with key codes
 	//  (search the web for "javascript key codes" for more examples)
-	// 
-	// KEY_FUNCTION can be any of the functions defined by 
+	//
+	// KEY_FUNCTION can be any of the functions defined by
 	// get_hotkeys_info() located in the file include/functions.php
 	//
-	// The default hotkey bindings are defined by 
+	// The default hotkey bindings are defined by
 	// get_hotkeys_map()  also located in the file include/functions.php
 
 	function hook_hotkey_map($hotkeys) {
-		$hotkeys["n"] = "next_article_noscroll"; 
-		$hotkeys["i"] = "prev_article_noscroll"; 
-		$hotkeys["u"] = "article_scroll_up";                                                                                    
-		$hotkeys["e"] = "article_scroll_down";          
+		$hotkeys["n"] = "next_article_noscroll";
+		$hotkeys["i"] = "prev_article_noscroll";
+		$hotkeys["u"] = "article_scroll_up";
+		$hotkeys["e"] = "article_scroll_down";
 		return $hotkeys;
 
 	}
